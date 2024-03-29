48dbba6a7235f3b0e65cdd9a0c234f74b3678636
xiang.xi <xxzzing@126.com>
2024-03-29 17:51:49 +0800

    2024-3-29

---
+++ pokeGame.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.32-log : Database - pokegame
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pokegame` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `pokegame`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` bigint(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`user_name`,`password`) values (1773172597455552514,'qwe','123'),(1773192802873327618,'asd','123'),(1773610954870427649,'zxc','123'),(1773611378339942401,'qq','123'),(1773611979828301825,'aa','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
