66db927291cb26cbe4342a23dc5af5242a6e65d2
xiang.xi <xxzzing@126.com>
2024-04-18 13:38:09 +0800

    修改ConverUtils“

--- myPokeGame/src/main/java/com/myPokeGame/utils/ConvertUtils.java
+++ myPokeGame/src/main/java/com/myPokeGame/utils/ConvertUtils.java
@@ -3,9 +3,23 @@ package com.myPokeGame.utils;
 import com.myPokeGame.entity.Message;
 import com.myPokeGame.entity.UnReadMessage;
 import com.myPokeGame.entity.User;
+import com.myPokeGame.mapper.MessageMapper;
+import com.myPokeGame.mapper.UserMapper;
 import com.myPokeGame.models.vo.MessageVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
+import java.util.*;
+
+@Component
 public class ConvertUtils {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     public static void convert(UnReadMessage unReadMessage,Message message){
         unReadMessage.setMessageId(message.getId());
@@ -20,5 +34,39 @@ public class ConvertUtils {
         vo.setMessageContent(mes.getContent());
         vo.setMessageId(mes.getId());
         vo.setDate(mes.getDate());
+    }
+
+    /**
+     * 转化方法
+     * @param messages
+     * @param ins 用于标识待转化的类型
+     * @return
+     */
+    public List<MessageVo> convert(Collection<Message> messages,MessageVo ins){
+        List<MessageVo> vos=new LinkedList<>();
+        Map<Long,User> userMap=new HashMap<>();
+        for(Message message:messages){
+            MessageVo vo=new MessageVo();
+            Long sendUserId = message.getSendUserId();
+            Long receiveUserId = message.getReceiveUserId();
+            vo.setSendUser(getUserFromMap(sendUserId,userMap));
+            vo.setReceiveUser(getUserFromMap(receiveUserId,userMap));
+            vo.setDate(message.getDate());
+            vo.setMessageId(message.getId());
+            vo.setMessageContent(message.getContent());
+            vos.add(vo);
+        }
+        userMap.clear();
+        return vos;
+    }
+
+    public User getUserFromMap(Long id,Map<Long,User> userMap){
+        if(!ObjectUtils.isEmpty(id)){
+            if(ObjectUtils.isEmpty(userMap.get(id))){
+                User user = userMapper.selectById(id);
+                userMap.put(id,user);
+            }
+        }
+        return userMap.get(id);
     }
 }
