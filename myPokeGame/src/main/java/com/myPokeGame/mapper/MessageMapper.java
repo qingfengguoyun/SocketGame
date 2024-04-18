b72273bb60d49be0d607af8c50b1967a65d1dd8e
xiang.xi <xxzzing@126.com>
2024-04-18 13:33:12 +0800

    2024-4-18 添加未读信息筛选，私聊信息查询方法

--- myPokeGame/src/main/java/com/myPokeGame/mapper/MessageMapper.java
+++ myPokeGame/src/main/java/com/myPokeGame/mapper/MessageMapper.java
@@ -3,6 +3,7 @@ package com.myPokeGame.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.myPokeGame.entity.Message;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,13 @@ public interface MessageMapper extends BaseMapper<Message> {
 
     public List<Message> queryLatestMessagesByDate(Integer num);
 
+    public List<Message> queryPrivateMessagesByUnReadMessageId(@Param("sendUserId") Long sendUserId,
+                                                               @Param("receiveUserId") Long receiveUserId,
+                                                               @Param("messageId") Long messageId,
+                                                               @Param("num")Integer num);
+
+    public List<Message> queryPrivateMessages(@Param("sendUserId") Long sendUserId,
+                                              @Param("receiveUserId") Long receiveUserId,
+                                              @Param("num") Integer num);
 
 }
--- myPokeGame/src/main/java/com/myPokeGame/mapper/UnReadMessageMapper.java
+++ myPokeGame/src/main/java/com/myPokeGame/mapper/UnReadMessageMapper.java
@@ -3,11 +3,15 @@ package com.myPokeGame.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.myPokeGame.entity.UnReadMessage;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Mapper
 public interface UnReadMessageMapper extends BaseMapper<UnReadMessage> {
 
-
+    UnReadMessage queryBySendUserIdAndReceiveUserId(@Param("sendUserId") Long sendUserId ,
+                                                          @Param("receiveUserId")Long receiveUserId);
 }
